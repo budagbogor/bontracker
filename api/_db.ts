97375759ba12ebdfbad1efea7b815066625698b6
budagbogor <budagbogor@gmail.com>
2026-05-20 05:13:23 +0700

    fix: fix Vercel serverless functions - inline schema, fix imports

--- api/_db.ts
+++ api/_db.ts
@@ -1,6 +1,6 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from '../src/db/schema';
+import * as schema from './_schema';
 
 const sqlClient = neon(process.env.DATABASE_URL!);
 export const db = drizzle(sqlClient, { schema });
